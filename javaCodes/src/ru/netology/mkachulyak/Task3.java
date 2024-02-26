c200e82d4b825fdd6b02942dfcf1a73e5d53fbc3
Margarita <119706111+m-kachulyak@users.noreply.github.com>
2024-02-26 22:48:16 +0300

    Add files via upload

---
+++ javaCodes/src/ru/netology/mkachulyak/Task3.java
@@ -0,0 +1,41 @@
+package ru.netology.mkachulyak;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] transactionTypes = new String[2];
+        double[] amounts = new double[2];
+        int[] transactionIds = new int[2];
+        LocalDate[] dates = new LocalDate[2];
+
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Введите тип транзакции " + (i + 1) + ": ");
+            transactionTypes[i] = scanner.next();
+
+            System.out.print("Введите сумму транзакции " + (i + 1) + ": ");
+            amounts[i] = scanner.nextDouble();
+
+            System.out.print("Введите ID транзакции " + (i + 1) + ": ");
+            transactionIds[i] = scanner.nextInt();
+
+            System.out.print("Введите дату транзакции " + (i + 1) + " в формате (ГГГГ-ММ-ДД): ");
+            String inputDate = scanner.next();
+            dates[i] = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        }
+
+        System.out.println("Информация о всех транзакциях:");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Транзакция " + (i + 1) + ": " + transactionTypes[i] + ", " + amounts[i] + ", " + transactionIds[i] + ", " + dates[i]);
+        }
+
+        scanner.close();
+    }
+}
+
+
