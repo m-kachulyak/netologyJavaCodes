ace054008505080c59d5ac0fcef2c2786ad71c99
Margarita <119706111+m-kachulyak@users.noreply.github.com>
2024-03-24 19:09:38 +0300

    Update StorageService.java

--- javaCodes/service/StorageService.java
+++ javaCodes/service/StorageService.java
@@ -3,13 +3,10 @@ package ru.netology.mkachulyak.Task_6_7_8_9.service;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Сервис для работы с хранением данных
- */
+
 public class StorageService<T> {
 
     private List<T> dataList = new ArrayList<T>();
-
 
     public void addData(T data) {
         dataList.add(data);
@@ -18,6 +15,4 @@ public class StorageService<T> {
     public List<T> getAllData() {
         return dataList;
     }
-
-
 }
